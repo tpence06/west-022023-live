825d953321697f442d354716fde4f4a9fd41f399
rothberry <phil.roth077@gmail.com>
2023-02-24 10:45:14 -0800

    dom events starter

---
+++ 1-phase/05-events/src/index.js
@@ -0,0 +1,24 @@
+// Code 👀
+/* 
+    ! DOM Events
+    
+    Warm Up Questions:
+    
+    1. What are JS Events?
+
+    2. List a few types of JS Events?
+
+    3. What are the 2 required arguments for .addEventLister()?
+
+*/
+
+/* 
+    Deliverables
+
+    1. (Review) Replace Content from the DOM list with book inventory
+    
+    2. Can Remove a book by clicking on it
+
+    3. Can Add a book through the form
+
+*/
