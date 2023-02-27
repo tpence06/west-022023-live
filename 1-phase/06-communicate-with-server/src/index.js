e3ae2ad85fc8272082b6e0e6525e59c43668e283
rothberry <phil.roth077@gmail.com>
2023-02-27 14:00:41 -0800

    fetch starter

---
+++ 1-phase/06-communicate-with-server/src/index.js
@@ -0,0 +1,33 @@
+// Code Here 👀👀
+
+/* 
+    ! Communicating with the Server
+
+    Warm Up Questions:
+
+    1. What is the Request/Reponse Cycle?
+
+    2. What is HTTP?
+
+    3. What are the main HTTP methods?
+        1. 
+        2. 
+        3. 
+        4. 
+        5. 
+        
+    4. RESTful Routing
+
+        // ! Allpertain to ONE class/model/type of thing
+        Ex => Stores
+
+        1. 
+        2. 
+        3. 
+        4. 
+        5.
+        6. 
+        7. 
+ */
+
+
