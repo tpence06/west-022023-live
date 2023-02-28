8ae201c2053cb4817e8c3407a4fff4ea5af3dd89
rothberry <phil.roth077@gmail.com>
2023-02-28 10:55:36 -0800

    POST starter

---
+++ 1-phase/08-post-patch-and-delete/src/index.js
@@ -0,0 +1,20 @@
+/* 
+    ! POST Requests and Forms!
+
+    Review Questions!
+
+    1. What are the main HTTP Methods?
+        1.
+        2.
+        3.
+        4.
+        5.
+
+    2. Name the 7 (or 5) RESTful Routes?
+        1.
+        2.
+        3.
+        4.
+        5.
+
+*/
